01825d32160a472e58e766fbc4c090e2adc1b5b9
CarlosHugoRodriguezC <cahuroca.aw@gmail.com>
2023-03-02 23:06:22 -0600

    tok tik app, video app scrolling

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tok_tik_app/config/theme/app_theme.dart';
+import 'package:tok_tik_app/presentation/providers/discover_provider.dart';
+import 'package:tok_tik_app/presentation/screens/discover/discover_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            // lazy: false,
+            create: (_) => DiscoverProvider()..loadNextPage()),
+      ],
+      child: MaterialApp(
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme().getTheme(),
+        home: const Scaffold(
+            // appBar: AppBar(
+            //   title: const Text('Material App Bar'),
+            // ),
+            body: DiscoverScreen()),
+      ),
+    );
+  }
+}
